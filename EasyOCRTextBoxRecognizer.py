21d414e76d53896f45f82ced6bb7f9f90d3a7aa4
Toshiyuki Arai <atlan@antillia.com>
2023-01-03 07:36:33 +0900

    Initial commit

---
+++ EasyOCRTextBoxRecognizer.py
@@ -0,0 +1,119 @@
+# Copyright 2023 antillia.com Toshiyuki Arai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# EasyOCRTextBoxRecognizer.py
+# 2022/10/20
+# toshiyuki.arai@xenera.jp
+# This works on Windows11
+
+import os
+import sys
+import time
+import glob
+#import csv
+#from PIL import ImageDraw, ImageFont
+import easyocr
+
+import numpy as np
+
+import traceback
+#from CosineSimilarity import CosineSimilarity
+from ConfigParser import ConfigParser
+from ImagePreprocessor import ImagePreprocessor
+
+from RecognizedObjectsVisualizer import RecognizedObjectsVisualizer
+
+class EasyOCRTextBoxRecognizer:
+  def __init__(self, config_file):
+    self.config    = ConfigParser(config_file)
+  
+    PARAMETER           = "parameter"
+    self.images_dir     = self.config.get(PARAMETER, "images_dir")
+    self.output_dir     = self.config.get(PARAMETER, "output_dir")
+    # ['ja','en']
+    self.language_hints = self.config.get(PARAMETER, "language_hints")
+    self.image_format   = self.config.get(PARAMETER, "image_format")
+    self.gpu            = self.config.get(PARAMETER, "gpu")
+    PREPROCESSOR        = "preprocessor"
+    self.preprocessing  = self.config.get(PREPROCESSOR, "preprocessing") 
+    self.image_scaling  = self.config.get(PREPROCESSOR, "image_scaling")
+    self.contrast       = self.config.get(PREPROCESSOR, "constrast")
+    self.gray_image     = self.config.get(PREPROCESSOR, "gray_image")
+    self.sharpness      = self.config.get(PREPROCESSOR, "sharpness")
+
+    VISUALIZER          = "visualizer"
+    self.font_name      = self.config.get(VISUALIZER, "font_name")
+    self.draw_boundingbox = self.config.get(VISUALIZER, "draw_boundingbox")
+
+    self.reader = easyocr.Reader(self.language_hints, gpu=self.gpu)
+
+    #self.similarity = CosineSimilarity()
+
+
+  def recognize(self): 
+    if not os.path.exists(self.images_dir):
+      raise Exception("Not found dir " + self.images_dir)
+
+    if not os.path.exists(self.output_dir):
+      os.makedirs(self.output_dir)  
+
+    image_files     = glob.glob(self.images_dir +  "./*" + self.image_format)
+    image_files     = sorted(image_files)
+    print("--- image files {}".format(image_files))
+    for image_file in image_files:
+      print("image_file {}".format(image_file))
+      self.recognize_one(image_file, self.output_dir)
+
+
+  def recognize_one(self, image_file, output_dir):
+    try:
+      
+      preprocessor = ImagePreprocessor(gray_image=self.gray_image, preprocessing=self.preprocessing)
+
+      img = preprocessor.read(image_file, image_scaling=self.image_scaling, 
+                              contrast=self.contrast, sharpness=self.sharpness)
+    
+      basename = os.path.basename(image_file)
+      if self.preprocessing:
+        save_image_name = "preprocessed_" + "scaling_" + str(self.image_scaling) + "_contrast_" + str(self.contrast) + "_sharpness_" + str(self.sharpness) + "_" +basename
+      else:
+        save_image_name = "non_preprocessed_" + basename
+
+      enhanced_image_file = os.path.join(output_dir, save_image_name)
+      img.save(enhanced_image_file) #, "PNG")
+      boxies = self.reader.readtext(enhanced_image_file)
+      if boxies !=None:
+        visualizer  = RecognizedObjectsVisualizer(self.font_name)
+        visualizer.visualize(boxies, enhanced_image_file, output_dir, self.draw_boundingbox)
+      
+    except:
+      traceback.print_exc()
+
+# 
+if __name__ == "__main__":
+  
+  config_file = "./recognition.conf"
+
+  try: 
+    start_time = time.time()
+
+    recognizer = EasyOCRTextBoxRecognizer(config_file)
+            
+    recognizer.recognize() 
+    end_time  = time.time()
+    ellapsed = round(end_time - start_time, 4)
+    print("--- time       = {}".format(ellapsed))
+  except:
+    traceback.print_exc()
--- README.md
+++ README.md
@@ -1,2 +1,114 @@
 # EasyOCRTextBoxRecognizer
-EasyOCR Text Box Recognizer
+EasyOCR Text Box Reconginizer
+
+This a simple command line client EasyOCR.<br>
+
+<h2> 
+1 Install 
+</h2>
+We use Python3 venv on Windows11 OS.
+Please create a python venv (virtual env).<br>
+<pre>
+python -m ven c:\py38-ocr
+</pre>
+, and run the following command to activate the venv.<br>
+<pre>
+c:\py38-ocr\scripts\activate
+</pre>
+
+
+<br>
+<h2>
+2 Install python packages
+</h2>
+<pre>
+pip install easyocr
+pip uinstall pip uninstall opencv-python-headless
+pip install -U opencv-python
+</pre>
+
+
+<h2>
+3 Sample Program
+</h2>
+Please open Windows Powershell console, and run the following command in the console window.<br>
+
+> python EasyOCRTextBoxRecognizer.py
+
+<br>
+This EasyOCRTextBoxRecognizer.py script reads the following recognition.conf file.<br>
+<pre>
+[parameter]
+images_dir       = "./samples"
+output_dir       = "./outputs"
+image_format     = ".png"
+gpu              = False
+language_hints   = ['ja','en']
+
+[preprocessor]
+preprocessing    = True
+gray_image       = True
+image_scaling    = 3
+constrast        = 2
+sharpness        = 3
+
+[visualizer]
+font_name        = "BIZ-UDMinchoM.ttc"
+draw_boundingbox = True
+
+</pre>
+
+Please note that we specify the language_hints in this config file to be "ja" to recognize Japanese text.<br>
+You have to change this language_hints property depending on your text language.<br>
+
+Example 1: 参考・教育漢字を除く常用漢字<br>
+<img src="./samples/Jyouyou-Kanji.png" width="1280" height="auto"><br>
+<br>
+
+
+Text Box Recognition:<br>
+<img src="./outputs/preprocessed_scaling_3_contrast_2_sharpness_3_Jyouyou-Kanji.png"
+     width="1280" height="auto">
+<br>
+
+Example 2: 付録常用漢字の一覧_付表<br>
+<img src="./samples/Jyouyo-Kanji_fuhyou.png" width="1280" height="auto"><br>
+<br>
+
+Text Box Recognition:<br>
+<img src="./outputs/preprocessed_scaling_3_contrast_2_sharpness_3_Jyouyo-Kanji_fuhyou.png"
+     width="1280" height="auto">
+<br>
+
+
+Example 3: 半角カタカナ一覧<br>
+<img src="./samples/Hankaku-katakana.png" width="1280" height="auto"><br>
+<br>
+
+
+Text Box Recognition:<br>
+<img src="./outputs/preprocessed_scaling_3_contrast_2_sharpness_3_Hankaku-katakana.png"
+     width="1280" height="auto">
+<br>
+
+Example 4: VSCodeScreenShot<br>
+<img src="./samples/VSCodeScreenShot.png" width="1280" height="auto"><br>
+<br>
+
+
+Text Box Recognition:<br>
+<img src="./outputs/preprocessed_scaling_3_contrast_2_sharpness_3_VSCodeScreenShot.png"
+     width="1280" height="auto">
+<br>
+
+Example 5: Symbols_Kakana<br>
+<img src="./samples/Symbols_Kakana.png" width="1280" height="auto"><br>
+<br>
+
+
+Text Box Recognition:<br>
+<img src="./outputs/preprocessed_scaling_3_contrast_2_sharpness_3_Symbols_Kakana.png"
+     width="1280" height="auto">
+<br>
+
+
